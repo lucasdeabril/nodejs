8d4e88df67caa7088da426f2f9805d3d039259a3
lucasdeabril <lucaspassosdesouza08@gmail.com>
2023-07-26 03:32:02 -0400

    atualização

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http');
 const PORT = 3000;
+const routes = express.Router()
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
 });
+
+routes.get('/', (req, res) => {
+  return res.json(db);
+  });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
